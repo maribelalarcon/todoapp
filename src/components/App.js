4cfb4fa1e9694861745ebc6f6ff8c1b51a4a897b
Maribel Alarcon <maribelalarcon@mbp-de-maribel.lan>
2022-10-18 22:29:59 -0300

    Cambiar de array a objeto, agregar todo, checkar todo

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,43 @@ import React, { useState } from "react";
 
 function App() {
   const [newTodo, setNewTodo] = useState("");
-  const [todos, setTodos] = useState([
-    {
+  const [todos, setTodos] = useState({
+    1: {
       id: 1,
       checked: false,
       text: "comprar leche",
     },
-    {
+    2: {
       id: 2,
       checked: true,
       text: "hacer la cama",
     },
-  ]);
+  });
 
   const onNewTodoChange = (event) => setNewTodo(event.target.value);
 
-  const onAddNewTodo = () =>
-    setTodos([
+  const onAddNewTodo = () => {
+    const id = Date.now();
+    setTodos({
       ...todos,
-      {
-        id: Date.now(),
+      [id]: {
+        id,
         checked: false,
         text: newTodo,
       },
-    ]);
+    });
+    setNewTodo("");
+  };
+
+  const onTodoChecked = (id) => (event) => {
+    setTodos({
+      ...todos,
+      [id]: {
+        ...todos[id],
+        checked: event.target.checked,
+      },
+    });
+  };
 
   return (
     <div className="App">
@@ -34,9 +47,13 @@ function App() {
       <button>Filter</button>
       <button>Cats</button>
       <ul>
-        {todos.map(({ id, text, checked }) => (
+        {Object.values(todos).map(({ id, text, checked }) => (
           <li key={id}>
-            <input type="checkbox" checked={checked} />
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={onTodoChecked(id)}
+            />
             {text}
             <button>X</button>
             <button>E</button>
